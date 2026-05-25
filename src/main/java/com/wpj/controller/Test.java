0593226104eb134e83511600fd8bd484fc5312df
Wang Pei Ji <15236100496@163.com>
2026-05-25 12:08:10 +0800

    这是看开发人员的第四次提交

--- src/main/java/com/wpj/controller/Test.java
+++ src/main/java/com/wpj/controller/Test.java
@@ -3,5 +3,6 @@ package com.wpj.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("这是一个用来测试的项目");
+        System.out.println("这是一个用来测试的项目");
     }
 }
