1a33c541d6eb2dfc418f679237b5bba04df271e5
Wang Pei Ji <15236100496@163.com>
2026-05-26 14:59:42 +0800

    这是开发人员的第七次提交

---
+++ how ea05e05 -- Test.java
@@ -0,0 +1,20 @@
+[33mcommit ea05e051cb219dfe3f5be8d7cc4451575a6e17c8[m
+Merge: 7ff10e1 6bb75e8
+Author: Wang Pei Ji <15236100496@163.com>
+Date:   Mon May 25 21:21:17 2026 +0800
+
+    这是看开发人员的第五次提交
+
+[1mdiff --cc src/main/java/com/wpj/controller/Test.java[m
+[1mindex b5eaaae,8000269..857346c[m
+[1m--- a/src/main/java/com/wpj/controller/Test.java[m
+[1m+++ b/src/main/java/com/wpj/controller/Test.java[m
+[36m@@@ -3,7 -3,6 +3,5 @@@[m [mpackage com.wpj.controller[m
+  public class Test {[m
+      public static void main(String[] args) {[m
+          System.out.println("这是一个用来测试的项目");[m
+[31m-         System.out.println("这是一个用来测试的项目");[m
+[31m-         System.out.println("这是看开发人员的第五次提交");[m
+[31m -        System.out.println("这是开发人员进行项目push");[m
+      }[m
+  }[m
--- src/main/java/com/wpj/controller/Test.java
+++ src/main/java/com/wpj/controller/Test.java
@@ -3,6 +3,7 @@ package com.wpj.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("这是一个用来测试的项目");
+        System.out.println("这是开发人员进行项目push");
         System.out.println("第七次提交");
     }
 }
